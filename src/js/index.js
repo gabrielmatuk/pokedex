345d1397fdec10333e00f50f314b12b1f7b1a2e3
Gabriel Matuk <gabrielmatuk14@gmail.com>
2022-03-13 18:47:53 -0300

    add js

---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+const listSelectedPokemon = document.querySelectorAll(".pokemon");
+const pokemonsCard = document.querySelectorAll(".card-pokemon");
+
+listSelectedPokemon.forEach((pokemon) => {
+  pokemon.addEventListener("click", () => {
+    const cardOpen = document.querySelector(".open");
+    cardOpen.classList.remove("open");
+
+    const idSelected = pokemon.attributes.id.value;
+
+    let selectedCard = `card-${idSelected}`;
+    const cardPokemonToOpen = document.getElementById(selectedCard);
+    console.log(idSelected);
+    cardPokemonToOpen.classList.add("open");
+
+    const activePokemon = document.querySelector(".active");
+    activePokemon.classList.remove("active");
+
+    const selectedListPokemon = document.getElementById(idSelected);
+    selectedListPokemon.classList.add("active");
+  });
+});
